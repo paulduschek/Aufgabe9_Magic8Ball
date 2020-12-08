588792732a56d2879e8bfaf6ebc6a743ce556710
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2020-12-08 21:56:21 +0100

    implemented constructor, getRandomNumber in MainModel.java

--- src/Main/MainModel.java
+++ src/Main/MainModel.java
@@ -1,9 +1,45 @@
 package Main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * @author Paul Duschek
  * @version 1.0, 8.12.2020
  */
 
 public class MainModel {
+
+    ArrayList<String> replies = new ArrayList<>();
+
+    public MainModel()
+    {
+        replies.add("It is certain.");
+        replies.add("It is decidedly so.");
+        replies.add("Without a doubt.");
+        replies.add("Yes – definitely.");
+        replies.add("You may rely on it.");
+        replies.add("As I see it, yes.");
+        replies.add("Most likely.");
+        replies.add("Outlook good.");
+        replies.add("Yes.");
+        replies.add("Signs point to yes.");
+        replies.add("Reply hazy, try again.");
+        replies.add("Ask again later.");
+        replies.add("Better not tell you now.");
+        replies.add("Cannot predict now.");
+        replies.add("Concentrate and ask again.");
+        replies.add("Don’t count on it.");
+        replies.add("My reply is no.");
+        replies.add("My sources say no.");
+        replies.add("Outlook not so good.");
+        replies.add("Very doubtful.");
+    }
+
+    public String getRandomNumber()
+    {
+        Random rd = new Random();
+        return replies.get(rd.nextInt(replies.size()));
+    }
 }
