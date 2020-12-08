8c18813156737b5bfbd8997827a1a5383bcd3fc3
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2020-12-08 18:39:29 +0100

    fully implemented method LoginModel.java

--- src/Login/LoginModel.java
+++ src/Login/LoginModel.java
@@ -1,9 +1,31 @@
 package Login;
 
+
 /**
  * @author Paul Duschek
  * @version 1.0, 8.12.2020
  */
 
 public class LoginModel {
+    private String UserName;
+    private String password;
+
+    public LoginModel()
+    {
+        UserName = "duschek.pau";
+        password = "HTLwels";
+    }
+
+    public boolean checkLogin(String username, String passwd)
+    {
+        if(username.equals(UserName) && passwd.equals(password))
+        {
+            return true;
+        }
+        else
+        {
+            System.out.println("False Username or password!");
+            return false;
+        }
+    }
 }
